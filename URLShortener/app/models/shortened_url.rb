b14f5d6cd8f72bbf52841c8bcdc35c366a7a78c0
ivank710 <ivan.kwong710@gmail.com>
2019-01-23 17:16:00 -0800

    Add associations has_many :visitors, :visits to ShortenedUrl class

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -19,6 +19,15 @@ class ShortenedUrl < ApplicationRecord
     foreign_key: :user_id,
     class_name: :User
 
+  has_many :visits,
+    primary_key: :id,
+    foreign_key: :shortened_url_id,
+    class_name: :Visit
+
+  has_many :visitors,
+    through: :visits,
+    source: :user
+
   def self.random_code
     unique = false
     until unique
